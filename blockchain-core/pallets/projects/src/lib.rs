396dfb1d9c189d0f610e9590230872125ebccef1
Nuno Cunha <nuno@exemplo.com>
2026-04-06 14:35:22 +0100

    Build attempt #24030751162 - Failed with E0277 in pallet-projects

--- blockchain-core/pallets/projects/src/lib.rs
+++ blockchain-core/pallets/projects/src/lib.rs
@@ -5,6 +5,8 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
     use frame_support::pallet_prelude::*;
+    use frame_support::BoundedVec;
+    use frame_support::traits::ConstU32;
     use frame_system::pallet_prelude::*;
 
     #[pallet::config]
@@ -15,6 +17,22 @@ pub mod pallet {
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
+
+    #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+    pub struct ContributionRecord<AccountId, Balance> {
+        pub account: AccountId,
+        pub project_id: u32,
+        pub amount: Balance,
+    }
+
+    #[pallet::storage]
+    pub type Contributions<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        u32,
+        BoundedVec<ContributionRecord<T::AccountId, u128>, ConstU32<100>>,
+        ValueQuery
+    >;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -36,8 +54,28 @@ pub mod pallet {
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
+
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        // As tuas funções (extrinsics) para criar e financiar projetos entram aqui.
+
+        #[pallet::weight(10_000)]
+        pub fn contribute(
+            origin: OriginFor<T>,
+            project_id: u32,
+            amount: u128,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            Contributions::<T>::try_mutate(project_id, |list| {
+                list.try_push(ContributionRecord {
+                    account: who.clone(),
+                    project_id,
+                    amount,
+                })
+            })?;
+
+            Ok(())
+        }
     }
 }
+
